78dbb123054925d9f94ed92b94e74ab42a0f2ad6
Victor Jiao <40229970+jiaoboxuan@users.noreply.github.com>
2023-03-25 14:01:08 -0500

    refactoring

--- pygame-example.py
+++ pygame-example.py
@@ -4,12 +4,13 @@ import random
 
 # Define the Ball class
 class Ball:
-    def __init__(self, screen_width, screen_height):
+    def __init__(self, screen_width, screen_height, color):
         self.radius = 15
         self.x = random.randint(self.radius, screen_width - self.radius)
         self.y = random.randint(self.radius, screen_height - self.radius)
         self.speed = 5
         self.angle = random.uniform(0, 2 * math.pi)
+        self.color = color
     
     def move(self):
         self.x += self.speed * math.cos(self.angle)
@@ -17,12 +18,18 @@ class Ball:
     
     def bounce(self, screen_width, screen_height):
         if self.x - self.radius < 0 or self.x + self.radius > screen_width:
-            self.angle = random.uniform(0, 2 * math.pi) - self.angle
+            self.angle = math.pi - self.angle
         if self.y - self.radius < 0 or self.y + self.radius > screen_height:
-            self.angle = random.uniform(0, 2 * math.pi) - self.angle
+            self.angle = -self.angle
     
     def draw(self, screen):
-        pygame.draw.circle(screen, (0, 0, 0), (int(self.x), int(self.y)), self.radius)
+        pygame.draw.circle(screen, self.color, (int(self.x), int(self.y)), self.radius)
+        
+    def random_color_tuple():
+        return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+
+
 
 # Initialize Pygame
 pygame.init()
@@ -38,8 +45,14 @@ pygame.display.set_caption("My Game")
 # Set up the clock
 clock = pygame.time.Clock()
 
-# Set up the ball
-ball = Ball(screen_width, screen_height)
+# Set up the balls
+balls = [
+    Ball(screen_width, screen_height, Ball.random_color_tuple()),
+    Ball(screen_width, screen_height, Ball.random_color_tuple()),
+    Ball(screen_width, screen_height, Ball.random_color_tuple()),
+    Ball(screen_width, screen_height, Ball.random_color_tuple()),
+    Ball(screen_width, screen_height, Ball.random_color_tuple()) 
+]
 
 # Set up the game loop
 running = True
@@ -49,15 +62,17 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
-    # Move and bounce the ball
-    ball.move()
-    ball.bounce(screen_width, screen_height)
+    # Move and bounce the balls
+    for ball in balls:
+        ball.move()
+        ball.bounce(screen_width, screen_height)
     
     # Draw the background image
     screen.blit(background_image, (0, 0))
     
-    # Draw the ball
-    ball.draw(screen)
+    # Draw the balls
+    for ball in balls:
+        ball.draw(screen)
     
     # Update the screen
     pygame.display.flip()
