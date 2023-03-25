630109c05ed8809832f732ab25bbc222ca14058e
Oliver Fong <oliverfong@Olivers-MacBook-Air.local>
2023-03-25 18:37:43 -0500

    added basket class, take_shot() method, and assignvalues method

--- player.py
+++ player.py
@@ -1,16 +1,27 @@
 import random
 import math
 import pygame
+import numpy as np
 
 # Define the Player class
 class Player:
-    def __init__(self, screen_width, screen_height, color):
+    def __init__(self, screen_width, screen_height, color, name, ):
         self.radius = 15
         self.x = random.randint(self.radius, screen_width - self.radius)
         self.y = random.randint(self.radius, screen_height - self.radius)
         self.speed = 5
         self.angle = random.uniform(0, 2 * math.pi)
         self.color = color
+        self.name = name
+    
+    def assignValues(self):
+        n = self.name
+        ndata = np.gettext("data/nba2021_advanced.csv", delimiter=',', usecols=(0,))
+        for i, name in enumerate(ndata, start=1):
+            if (n == name):
+                self.nbapos, self.age, self.tm, self.g, self.mp, self.per, self.ts, self.tsp, self.ftr= np.genfromtxt('data.csv', delimiter=',', skip_header=1, max_rows=1, usecols=(1,2,3,4,5,6,7,8,9), unpack=True)
+
+
     
     def move(self):
         self.x += self.speed * math.cos(self.angle)
